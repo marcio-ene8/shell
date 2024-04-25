c7bb027051debcbd366af64feb807d71407e772e
marcio-ene <33833468+marcio-ene8@users.noreply.github.com>
2024-04-25 10:21:58 -0300

    forecast_preview_df_v1.0.4
    forecast_preview_df_v1.0.4

--- forecast_preview_df.sh
+++ forecast_preview_df.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # sed -i -e 's/\r$//' scriptname.sh
-# forecast_preview_df.sh - Retorna a temperatura atual de Brasilia
+# forecast_preview_df.sh - Retorna a temperatura atual de Brasilia na tela
 #
-# Site:       https://
-# Autor:      Marcio Basilio
+# Site: https://
+# Autor: Marcio Basilio
 # Manutenção: Marcio Basilio
 #
 # ------------------------------------------------------------------------ #
@@ -12,11 +12,11 @@
 # ------------------------------------------------------------------------ #
 # Histórico:
 #
-#   v1.0 12/04/2024, Marcio Basilio:
-#       - extração dados clima-web
+# v1.0.3 12/04/2024, Marcio Basilio:
+# - extração dados clima-web
 # ------------------------------------------------------------------------ #
 # ------------------------------- VARIÁVEIS ------------------------------ #
-VERSAO="v1.0.2"
+VERSAO="v1.0.4"
 # ------------------------------------------------------------------------ #
 
 # ------------------------------- TESTES --------------------------------- #
@@ -30,6 +30,10 @@ VERSAO="v1.0.2"
 # ------------------------------- EXECUÇÃO ------------------------------- #
 curl wttr.in/Brasilia --output clima_df.txt
 grep °C clima_df.txt > forecast_df.txt
-obs_temp=$(head -1 forecast_df.txt | sed 's/.*+//;s/(.*C//')
-echo "temperatura atual: $obs_temp °C"
+#obs_temp=$(cat forecast_df.txt | head -1 | cut -d "." -f2 | cut -d "C" -f1 | sed 's/°.*//' | xargs)
+#obs_temp=$(cat forecast_df.txt | head -1 | cut -d "." -f2 | xargs)
+#obs_temp=$(head -1 forecast_df.txt | sed 's/.*+//;s/(.*C//')
+obs_temp=$(cat forecast_df.txt | sed 's/_.*-.//' | head -1 | xargs)
+echo "temperatura atual: $obs_temp"
 rm forecast_df.txt clima_df.txt
+echo $VERSAO
